434ff19588384319276124d1aeadd1ae4ef73b35
anaparty <saratchand.a@bahwaniton.com>
2015-05-03 01:24:59 +0400

    Initial Commit

---
+++ CarVoter/Model/Car.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace CarVoter.Model
+{
+    class Car
+    {
+        public BitmapImage Image { get; set; }
+        public string Title  { get; set; }
+        public string Description { get; set; }
+        public int NumDownVotes { get; set; }
+        public int NumUpVotes { get; set; }
+        public int KiloWats  { get; set; }
+    }
+}
---
+++ CarVoter/View/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Phone.Controls;
+
+namespace CarVoter
+{
+    public partial class MainPage 
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+         
+    }
+}

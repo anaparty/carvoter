a83aad1a9857648b7d9d3286586bf928fd668dcb
anaparty <saratchand.a@bahwaniton.com>
2015-05-03 01:31:08 +0400

    Added Loaded Events in Initailize Component

--- CarVoter/View/MainPage.xaml.cs
+++ CarVoter/View/MainPage.xaml.cs
@@ -11,6 +11,13 @@ namespace CarVoter
 
             // Sample code to localize the ApplicationBar
             //BuildLocalizedApplicationBar();
+
+            Loaded += MainPage_Loaded;
+        }
+
+        void MainPage_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
          
     }
